b1dfcb4e5d0638963116f9bc9a0a9e676d538331
Fernando William <fwsouz@gmail.com>
2025-04-10 18:24:57 -0300

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Renata, eu amo você. Se voce me ama, dá uma risadinha!</h1>
+    return <h1>Renata, eu amo você. Se voce me ama, dá uma risadinha! ;-)</h1>
 }
 
 export default Home;
