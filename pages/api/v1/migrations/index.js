ef07ed65a3acab49dcc231f5f6ec86b6cdb54c20
Fernando William <fwsouz@gmail.com>
2025-05-23 07:48:50 -0300

    fix: adjust in migrations to user controller correctly

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -9,51 +9,50 @@ const router = createRouter();
 router.get(getHandler);
 router.post(postHandler);
 
-export default router.handler({
-  onNoMatch: controller.onNoMatchHandler,
-  onError: controller.onErrorHandler,
-});
+export default router.handler(controller.errorHandlers);
+
+const defaultMigrationOptions = {
+  dryRun: true,
+  dir: resolve("infra", "migrations"),
+  direction: "up",
+  verbose: true,
+  migrationsTable: "pgmigrations",
+};
 
 async function getHandler(request, response) {
   let dbClient;
 
-  dbClient = await database.getNewClient();
+  try {
+    dbClient = await database.getNewClient();
 
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+    const pendingMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+    });
 
-  const pendingMigrations = await migrationRunner(defaultMigrationOptions);
-
-  return response.status(200).json(pendingMigrations);
+    return response.status(200).json(pendingMigrations);
+  } finally {
+    await dbClient?.end();
+  }
 }
 
 async function postHandler(request, response) {
   let dbClient;
 
-  dbClient = await database.getNewClient();
+  try {
+    dbClient = await database.getNewClient();
 
-  const defaultMigrationOptions = {
-    dbClient: dbClient,
-    dryRun: true,
-    dir: resolve("infra", "migrations"),
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationOptions,
+      dbClient,
+      dryRun: false,
+    });
 
-  const migratedMigrations = await migrationRunner({
-    ...defaultMigrationOptions,
-    dryRun: false,
-  });
-
-  if (migratedMigrations.length > 0) {
-    return response.status(201).json(migratedMigrations);
+    if (migratedMigrations.length > 0) {
+      return response.status(201).json(migratedMigrations);
+    }
+    response.status(200).json(migratedMigrations);
+  } finally {
+    await dbClient?.end();
   }
-  response.status(200).json(migratedMigrations);
 }
